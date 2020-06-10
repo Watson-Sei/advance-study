655ae1fbff1a93fb7d244bf96291b7ce4662fe4b
SEI <seinabehack@gmail.com>
2020-06-10 07:36:56 +0000

    second fix

--- culc.py
+++ culc.py
@@ -8,11 +8,12 @@ with open ('04data.csv', 'r',encoding='cp932') as f:
     for row in reader:
         if row[2] in t:
             t[row[2]]['point'] = t[row[2]]['point'] + int(row[3])
+            t[row[2]]['point-second'] = t[row[2]]['point-second'] + int(row[4])
             t[row[2]]['count'] = t[row[2]]['count'] + 1
         else:
-            t[row[2]] = {'point':int(row[3]), 'count':1}
+            t[row[2]] = {'point':int(row[3]), 'point-second':int(row[4]),'count':1}
  
 change = sorted(t)
 for i in change:
-    print(i, t[i]['point'] / t[i]['count'])
+    print(i, t[i]['point'] / t[i]['count'], t[i]['point-second'] / t[i]['count'])
     
