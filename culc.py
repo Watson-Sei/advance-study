bfdad80339ba2e1f5bcdbddc355286b0ee3b4670
SEI <seinabehack@gmail.com>
2020-06-10 07:29:13 +0000

    first commit

---
+++ culc.py
@@ -0,0 +1,18 @@
+import csv 
+
+t = {}
+
+with open ('04data.csv', 'r',encoding='cp932') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        if row[2] in t:
+            t[row[2]]['point'] = t[row[2]]['point'] + int(row[3])
+            t[row[2]]['count'] = t[row[2]]['count'] + 1
+        else:
+            t[row[2]] = {'point':int(row[3]), 'count':1}
+ 
+change = sorted(t)
+for i in change:
+    print(i, t[i]['point'] / t[i]['count'])
+    
